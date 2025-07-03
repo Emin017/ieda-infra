1664703aea338c87bcc990909c80bb918d157c9c
Qiming Chu <cchuqiming@gmail.com>
2025-07-03 16:36:47 +0800

    magic-vlsi: fix darwin
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- nix/pkgs/magic-vlsi/default.nix
+++ nix/pkgs/magic-vlsi/default.nix
@@ -7,11 +7,13 @@
   m4,
   cairo,
   libX11,
+  mesa,
   mesa_glu,
   ncurses,
   tcl,
   tcsh,
   tk,
+  fixDarwinDylibNames,
 }:
 
 stdenv.mkDerivation rec {
@@ -26,15 +28,20 @@ stdenv.mkDerivation rec {
     leaveDotGit = true;
   };
 
-  nativeBuildInputs = [
-    python3
-    git
-  ];
+  nativeBuildInputs =
+    [
+      python3
+      git
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      fixDarwinDylibNames
+    ];
 
   buildInputs = [
     cairo
     libX11
     m4
+    mesa
     mesa_glu
     ncurses
     tcl
@@ -55,32 +62,34 @@ stdenv.mkDerivation rec {
   '';
 
   postInstall = ''
-    # Create the sys directory
-    mkdir -p ${placeholder "out"}/lib/magic/sys
+    # Fix necessary files missing in sys directory
+    mkdir -p $out/lib/magic/sys
 
-    # Install all technology files that were generated
+    shopt -s nullglob
+
     for techfile in scmos/*.tech; do
-      if [ -f "$techfile" ]; then
-        install -Dm644 "$techfile" ${placeholder "out"}/lib/magic/sys/$(basename "$techfile")
-      fi
+      install -Dm644 "$techfile" $out/lib/magic/sys/$(basename "$techfile")
     done
 
-    # Install all display styles
     for dstylefile in scmos/*.dstyle; do
-      if [ -f "$dstylefile" ]; then
-        install -Dm644 "$dstylefile" ${placeholder "out"}/lib/magic/sys/$(basename "$dstylefile")
-      fi
+      install -Dm644 "$dstylefile" $out/lib/magic/sys/$(basename "$dstylefile")
     done
 
-    # Install all color maps
     for cmapfile in scmos/*.cmap; do
-      if [ -f "$cmapfile" ]; then
-        install -Dm644 "$cmapfile" ${placeholder "out"}/lib/magic/sys/$(basename "$cmapfile")
-      fi
+      install -Dm644 "$cmapfile" $out/lib/magic/sys/$(basename "$cmapfile")
     done
   '';
 
+  postFixup = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    # Fix dylib paths on macOS
+    install_name_tool -add_rpath ${mesa.out}/lib $out/lib/magic/tcl/tclmagic.dylib
+    install_name_tool -add_rpath ${mesa_glu.out}/lib $out/lib/magic/tcl/tclmagic.dylib
+    install_name_tool -add_rpath ${mesa.out}/lib $out/lib/magic/tcl/magicexec
+    install_name_tool -add_rpath ${mesa_glu.out}/lib $out/lib/magic/tcl/magicexec
+  '';
+
   env.NIX_CFLAGS_COMPILE = "-Wno-implicit-function-declaration";
+  env.NIX_LDFLAGS = lib.optionalString stdenv.hostPlatform.isDarwin "-headerpad_max_install_names";
 
   meta = with lib; {
     description = "VLSI layout tool written in Tcl";
